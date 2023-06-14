22879aa49ab05d29d20d9118dab7a44f9268ddfa
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2023-06-14 19:26:58 +0100

    test04

--- static/portfolio/js/imgTextInsp.js
+++
@@ -1,8 +0,0 @@
-const inspirationalButton = document.querySelector('#inspirational-button');
-const inspirationalImage = document.querySelector('#inspirational-image');
-const inspirationalPhrase = document.createElement('p');
-inspirationalButton.addEventListener('click', () => {
-  const phrase = prompt('Enter an inspiring phrase:');
-  inspirationalPhrase.textContent = phrase;
-  inspirationalImage.insertAdjacentElement('beforebegin', inspirationalPhrase);
-});
--- static/portfolio/js/onmouse.js
+++
@@ -1,7 +0,0 @@
-function mostrarMensagem() {
-    alert("Clica no botão e escreve uma frase inspiracional que te venha à cabeça enquanto olhas para a imagem que está acima!");
-  }
-  
-  function esconderMensagem() {
-    alert("Obrigado!");
-  }
